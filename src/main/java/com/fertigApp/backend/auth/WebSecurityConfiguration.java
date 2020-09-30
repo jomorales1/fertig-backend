a520d2c2adc0cdffe29f011ec20bda6aa6caa198
Santiago <crvargaso@unal.edu.co>
2020-09-29 23:26:41 -0500

    arreglo de cors para comunicación con el frontend

--- src/main/java/com/fertigApp/backend/auth/WebSecurityConfiguration.java
+++ src/main/java/com/fertigApp/backend/auth/WebSecurityConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -42,7 +44,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
                 roles("USER").build();
         return new InMemoryUserDetailsManager(user); //userAdmin
     }
-
+    @Override
+    public void configure(final WebSecurity web) {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
