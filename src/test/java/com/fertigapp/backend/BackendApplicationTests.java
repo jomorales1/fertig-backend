7348489bbb16d6305f1f8330903060399fe747de
Santiago <crvargaso@unal.edu.co>
2020-10-05 17:53:07 -0500

    Test Unitario

--- src/test/java/com/fertigapp/backend/BackendApplicationTests.java
+++ src/test/java/com/fertigapp/backend/BackendApplicationTests.java
@@ -1,13 +1,26 @@
 package com.fertigapp.backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootConfiguration
 @SpringBootTest
 class BackendApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplicationTests.class, args);
+    }
 
     @Test
-    void contextLoads() {
+    public void authLoginOk() throws Exception{
+        assertTrue(true);
     }
 
 }
