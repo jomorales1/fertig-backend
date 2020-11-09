7e6dde5df72389decd0d2833e03c60d68a2510ab
Jorge Morales <jomorales@unal.edu.co>
2020-11-08 20:41:52 -0500

    Sonidos default

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,29 @@
 INSERT INTO mydb.sonido (id_sonido)
-SELECT * FROM (SELECT "classical") AS tmp
+SELECT * FROM (SELECT "belonging") AS tmp
 WHERE NOT EXISTS (
-	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "classical"
+	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "belonging"
 ) LIMIT 1;
 
 INSERT INTO mydb.sonido (id_sonido)
-SELECT * FROM (SELECT "nature") AS tmp
+SELECT * FROM (SELECT "chilled") AS tmp
 WHERE NOT EXISTS (
-	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "nature"
+	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "chilled"
 ) LIMIT 1;
 
 INSERT INTO mydb.sonido (id_sonido)
-SELECT * FROM (SELECT "electronic") AS tmp
+SELECT * FROM (SELECT "dusk") AS tmp
 WHERE NOT EXISTS (
-	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "electronic"
+	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "dusk"
 ) LIMIT 1;
 
 INSERT INTO mydb.sonido (id_sonido)
-SELECT * FROM (SELECT "rain") AS tmp
+SELECT * FROM (SELECT "staring") AS tmp
 WHERE NOT EXISTS (
-	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "rain"
+	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "staring"
+) LIMIT 1;
+
+INSERT INTO mydb.sonido (id_sonido)
+SELECT * FROM (SELECT "theworld") AS tmp
+WHERE NOT EXISTS (
+	SELECT id_sonido FROM mydb.sonido WHERE id_sonido = "theworld"
 ) LIMIT 1;
