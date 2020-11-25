d666b91a9d17ed4ef87530be363b4c6f24ff10bc
juacostar <juacostar@unal.edu.co>
2020-11-24 21:02:39 -0500

    arreglo error de simon

--- src/main/java/com/fertigApp/backend/controller/ReporteController.java
+++ src/main/java/com/fertigApp/backend/controller/ReporteController.java
@@ -12,11 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -37,56 +35,62 @@ public class ReporteController {
     }
 
     @GetMapping(path="report/month")
-    public ResponseEntity<Reporte> getReporteMensual(@RequestBody RequestDate requestDate) {
+    public ResponseEntity<Reporte> getReporteMensual(@RequestParam(value ="fecha") String date) {
+        OffsetDateTime requestDate = OffsetDateTime.parse(date);
         Object principal =  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetails userDetails = (UserDetails) principal;
         Optional<Usuario> optUsuario =usuarioService.findById(userDetails.getUsername());
         ReporteMensualBuilder reporteMensualBuilder =  new ReporteMensualBuilder(this.tareaService, this.completadaService, this.tiempoService);
-        return ResponseEntity.ok(reporteMensualBuilder.crearReporte(requestDate.getFecha(),optUsuario.orElse(null)));
+        return ResponseEntity.ok(reporteMensualBuilder.crearReporte(requestDate,optUsuario.orElse(null)));
     }
 
     @GetMapping(path="report/week")
-    public ResponseEntity<Reporte> getReporteSemanal(@RequestBody RequestDate requestDate) {
+    public ResponseEntity<Reporte> getReporteSemanal(@RequestParam(value ="fecha") String date) {
+        OffsetDateTime requestDate = OffsetDateTime.parse(date);
         Object principal =  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetails userDetails = (UserDetails) principal;
         Optional<Usuario> optUsuario =usuarioService.findById(userDetails.getUsername());
         ReporteSemanalBuilder reporteSemanalBuilder =  new ReporteSemanalBuilder(this.tareaService, this.completadaService, this.tiempoService);
-        return ResponseEntity.ok(reporteSemanalBuilder.crearReporte(requestDate.getFecha(),optUsuario.orElse(null)));
+        return ResponseEntity.ok(reporteSemanalBuilder.crearReporte(requestDate,optUsuario.orElse(null)));
     }
 
     @GetMapping(path="report/year")
-    public ResponseEntity<Reporte> getReporteAnual(@RequestBody RequestDate requestDate) {
+    public ResponseEntity<Reporte> getReporteAnual(@RequestParam(value ="fecha") String date) {
+        OffsetDateTime requestDate = OffsetDateTime.parse(date);
         Object principal =  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetails userDetails = (UserDetails) principal;
         Optional<Usuario> optUsuario = usuarioService.findById(userDetails.getUsername());
         ReporteAnualBuilder reporteAnualBuilder =  new ReporteAnualBuilder(this.tareaService, this.completadaService, this.tiempoService);
-        return ResponseEntity.ok(reporteAnualBuilder.crearReporte(requestDate.getFecha(),optUsuario.orElse(null)));
+        return ResponseEntity.ok(reporteAnualBuilder.crearReporte(requestDate,optUsuario.orElse(null)));
     }
 
     @GetMapping(path="graphic/month")
-    public ResponseEntity<Grafica> getGraficaMensual(@RequestBody RequestDate requestDate) {
+    public ResponseEntity<Grafica> getGraficaMensual(@RequestParam(value ="fecha") String date) {
+        OffsetDateTime requestDate = OffsetDateTime.parse(date);
         Object principal =  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetails userDetails = (UserDetails) principal;
         Optional<Usuario> optUsuario =usuarioService.findById(userDetails.getUsername());
         GraficaMensualBuilder graficaMensualBuilder =  new GraficaMensualBuilder(this.tareaService, this.completadaService, this.tiempoService);
-        return ResponseEntity.ok(graficaMensualBuilder.crearGrafica(requestDate.getFecha(),optUsuario.orElse(null)));
+        return ResponseEntity.ok(graficaMensualBuilder.crearGrafica(requestDate,optUsuario.orElse(null)));
     }
 
     @GetMapping(path="graphic/week")
-    public ResponseEntity<Grafica> getGraficaSemanal(@RequestBody RequestDate requestDate) {
+    public ResponseEntity<Grafica> getGraficaSemanal(@RequestParam(value ="fecha") String date) {
+        OffsetDateTime requestDate = OffsetDateTime.parse(date);
         Object principal =  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetails userDetails = (UserDetails) principal;
         Optional<Usuario> optUsuario =usuarioService.findById(userDetails.getUsername());
         GraficaSemanalBuilder graficaSemanalBuilder =  new GraficaSemanalBuilder(this.tareaService, this.completadaService, this.tiempoService);
-        return ResponseEntity.ok(graficaSemanalBuilder.crearGrafica(requestDate.getFecha(),optUsuario.orElse(null)));
+        return ResponseEntity.ok(graficaSemanalBuilder.crearGrafica(requestDate,optUsuario.orElse(null)));
     }
 
     @GetMapping(path="graphic/year")
-    public ResponseEntity<Grafica> getGraficaAnual(@RequestBody RequestDate requestDate) {
+    public ResponseEntity<Grafica> getGraficaAnual(@RequestParam(value ="fecha") String date) {
+        OffsetDateTime requestDate = OffsetDateTime.parse(date);
         Object principal =  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetails userDetails = (UserDetails) principal;
         Optional<Usuario> optUsuario = usuarioService.findById(userDetails.getUsername());
         GraficaAnualBuilder graficaAnualBuilder =  new GraficaAnualBuilder(this.tareaService, this.completadaService, this.tiempoService);
-        return ResponseEntity.ok(graficaAnualBuilder.crearGrafica(requestDate.getFecha(),optUsuario.orElse(null)));
+        return ResponseEntity.ok(graficaAnualBuilder.crearGrafica(requestDate,optUsuario.orElse(null)));
     }
 }
